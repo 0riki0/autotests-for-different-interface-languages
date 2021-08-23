f061eafebf4babb3d85e88702a6593721ce3c07b
0riki0 <38318473+0riki0@users.noreply.github.com>
2021-08-23 16:38:27 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -10,7 +10,7 @@ def test_check_the_add_to_cart_button(browser):
     lang = browser.find_elements_by_css_selector("[lang='fr']")
     if lang == "fr":
         button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
-        assert button.text == "Ajouter au panier", f"!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ!!!!!!"
+        assert button.text == "Ajouter au panier", "!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ !!!!!!"
     else:
         button = browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
-        assert button, '!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ!!!!!!'
+        assert button, '!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ !!!!!!'
