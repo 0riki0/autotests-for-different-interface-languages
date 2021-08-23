1d164fd0564496f0c9dd779f0e75e4423fc1db84
riki <you@example.com>
2021-08-23 15:50:53 +0300

    Выполнение задания согласно требованиям

--- test_items.py
+++ test_items.py
@@ -1,1 +1,16 @@
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_the_add_to_cart_button(browser):
+    browser.get(link)
+    time.sleep(30)
+
+    lang = browser.find_elements_by_css_selector("[lang='fr']")
+    if lang == "fr":
+        button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+        assert button.text == "Ajouter au panier", f"!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ!!!!!!"
+    else:
+        button = browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+        assert button, '!!! НЕ НАШЕЛ КНОПКУ ДОБАВЛЕНИЯ В КОРЗИНУ!!!!!!'
